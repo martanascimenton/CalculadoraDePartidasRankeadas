9efd8a3c0cc370fffecae35d4fba8319ba386218
Marta Menezes <nascimentomenezesmarta@gmail.com>
2024-01-20 12:57:17 -0300

    Iniciando repositorio

---
+++ index.js
@@ -0,0 +1,32 @@
+function calcularRanking(vitorias, derrotas) {
+    const saldoVitorias = vitorias - derrotas;
+    let nivel = '';
+
+    if (vitorias < 10) {
+        nivel = 'Ferro';
+    } else if (vitorias >= 11 && vitorias <= 20) {
+        nivel = 'Bronze';
+    } else if (vitorias >= 21 && vitorias <= 50) {
+        nivel = 'Prata';
+    } else if (vitorias >= 51 && vitorias <= 80) {
+        nivel = 'Ouro';
+    } else if (vitorias >= 81 && vitorias <= 90) {
+        nivel = 'Diamante';
+    } else if (vitorias >= 91 && vitorias <= 100) {
+        nivel = 'Lendário';
+    } else {
+        nivel = 'Imortal';
+    }
+
+    return {
+        saldoVitorias: saldoVitorias,
+        nivel: nivel
+    };
+}
+
+
+const vitorias = 60;
+const derrotas = 20;
+const resultado = calcularRanking(vitorias, derrotas);
+
+console.log(`O Herói tem saldo de ${resultado.saldoVitorias} está no nível de ${resultado.nivel}`);
